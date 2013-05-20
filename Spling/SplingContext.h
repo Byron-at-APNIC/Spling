feb2ebc424a152bd99f03b2e05aa32201101ea75
Byron Ellacott <bje@the-wanderers.org>
2013-05-20 18:14:39 +1000

    Local changes

---
+++ Spling/SplingContext.h
@@ -0,0 +1,13 @@
+//
+//  SplingContext.h
+//  Spling
+//
+//  Created by Byron Ellacott on 18/05/13.
+//  Copyright (c) 2013 The Wanderers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SplingContext <NSObject>
+
+@end
